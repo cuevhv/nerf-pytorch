8b56e75274671d4663d66e380a9f9044a48d751c
cuevhv <cuevhv@yahoo.com>
2023-01-02 15:53:36 +0000

    added load_nerface_data

--- nerf/__init__.py
+++ nerf/__init__.py
@@ -1,7 +1,9 @@
 from .cfgnode import CfgNode
 from .load_blender import load_blender_data
+from .load_nerface import load_nerface_data
 from .load_llff import load_llff_data
 from .models import *
 from .nerf_helpers import *
+from .nerface_helpers import get_ray_bundle as get_ray_bundle_nerface
 from .train_utils import *
 from .volume_rendering_utils import *
