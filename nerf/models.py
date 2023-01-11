90e1b957936de2dc02db8904b3ed5060fb0a08c1
cuevhv <cuevhv@yahoo.com>
2023-01-11 16:28:04 +0000

    modified facenerfpapernerfmodel to our network

--- nerf/models.py
+++ nerf/models.py
@@ -380,47 +380,68 @@ class FaceNerfPaperNeRFModel(torch.nn.Module):
         include_input_xyz=True,
         include_input_dir=True,
         use_viewdirs=True,
-        include_expression=True,
-        latent_code_dim=32
+        use_expression=True,
+        use_landmarks3d: bool = True,
+        use_appearance_code: bool =True,
+        num_train_images: int = 0,
+        embedding_vector_dim=32
 
     ):
         super(FaceNerfPaperNeRFModel, self).__init__()
 
         include_input_xyz = 3 if include_input_xyz else 0
         include_input_dir = 3 if include_input_dir else 0
-        include_expression = 76 if include_expression else 0
+
+        include_expression = 50 if use_expression else 0
+        include_landmarks3d = 68 if use_landmarks3d else 0
 
         self.dim_xyz = include_input_xyz + 2 * 3 * num_encoding_fn_xyz
         self.dim_dir = include_input_dir + 2 * 3 * num_encoding_fn_dir
         self.dim_expression = include_expression# + 2 * 3 * num_encoding_fn_expr
-        self.dim_latent_code = latent_code_dim
+        self.dim_landmarks3d = include_landmarks3d + 2 * include_landmarks3d * num_encoding_fn_dir + include_landmarks3d * 3
+        
+        # add appearance code
+        self.use_appearance_code = use_appearance_code
+        self.dim_appearance_codes = embedding_vector_dim if use_appearance_code else 0
+        # self.dim_latent_code = embedding_vector_dim
 
         self.layers_xyz = torch.nn.ModuleList()
         self.use_viewdirs = use_viewdirs
-        self.layers_xyz.append(torch.nn.Linear(self.dim_xyz + self.dim_expression + self.dim_latent_code, 256))
+        self.use_landmarks3d = use_landmarks3d
+        self.layers_xyz.append(torch.nn.Linear(self.dim_landmarks3d + self.dim_xyz + self.dim_expression, 256))
         for i in range(1, 6):
             if i == 3:
-                self.layers_xyz.append(torch.nn.Linear(self.dim_xyz + self.dim_expression + self.dim_latent_code + 256, 256))
+                self.layers_xyz.append(torch.nn.Linear(self.dim_landmarks3d + self.dim_xyz + self.dim_expression + 256, 256))
             else:
                 self.layers_xyz.append(torch.nn.Linear(256, 256))
         self.fc_feat = torch.nn.Linear(256, 256)
         self.fc_alpha = torch.nn.Linear(256, 1)
 
         self.layers_dir = torch.nn.ModuleList()
-        self.layers_dir.append(torch.nn.Linear(256 + self.dim_dir, 128))
+        self.layers_dir.append(torch.nn.Linear(256 + self.dim_dir + self.dim_appearance_codes, 128))
         for i in range(3):
             self.layers_dir.append(torch.nn.Linear(128, 128))
         self.fc_rgb = torch.nn.Linear(128, 3)
         self.relu = torch.nn.functional.relu
 
-    def forward(self, x,  expr=None, latent_code=None, **kwargs):
-        xyz, dirs = x[..., : self.dim_xyz], x[..., self.dim_xyz :]
+    def forward(self, x,  expression=None, appearance_codes=None, **kwargs):
+        if self.use_landmarks3d:
+            xyz, dirs = x[..., : self.dim_landmarks3d+self.dim_xyz], x[..., self.dim_landmarks3d+self.dim_xyz :]
+        elif self.use_viewdirs:
+            xyz, dirs = x[..., : self.dim_xyz], x[..., self.dim_xyz :]
+        else:
+            xyz = x[..., : self.dim_xyz]
+        
         x = xyz#self.relu(self.layers_xyz[0](xyz))
-        latent_code = latent_code.repeat(xyz.shape[0], 1)
+        # appearance_codes = appearance_codes.repeat(xyz.shape[0], 1)
         if self.dim_expression > 0:
-            expr_encoding = (expr * 1 / 3).repeat(xyz.shape[0], 1)
-            initial = torch.cat((xyz, expr_encoding, latent_code), dim=1)
+            expr_encoding = (expression * 1 / 3).repeat(xyz.shape[0], 1)
+            initial = torch.cat((xyz, expr_encoding), dim=1)
+            # initial = torch.cat((xyz, expr_encoding, appearance_codes), dim=1)
             x = initial
+        else:
+            initial = xyz
+
         for i in range(6):
             if i == 3:
                 x = self.layers_xyz[i](torch.cat((initial, x), -1))
@@ -429,8 +450,13 @@ class FaceNerfPaperNeRFModel(torch.nn.Module):
             x = self.relu(x)
         feat = self.fc_feat(x)
         alpha = self.fc_alpha(feat)
+
         if self.use_viewdirs:
-            x = self.layers_dir[0](torch.cat((feat, dirs), -1))
+            if self.use_appearance_code:
+                appearance_codes = appearance_codes.repeat(xyz.shape[0], 1)
+                x = self.layers_dir[0](torch.cat((feat, dirs, appearance_codes), -1))
+            else:
+                x = self.layers_dir[0](torch.cat((feat, dirs), -1))
         else:
             x = self.layers_dir[0](feat)
         x = self.relu(x)
