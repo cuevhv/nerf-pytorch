39497ef135cc9ca39a7a2391ab27634b1345d6b0
cuevhv <cuevhv@yahoo.com>
2023-02-07 13:17:05 +0000

    create model facenerfpapernerfModelDualCond

--- nerf/models.py
+++ nerf/models.py
@@ -701,3 +701,175 @@ class FaceNerfPaperNeRFModelCond(torch.nn.Module):
         rgb = self.fc_rgb(x)
         return torch.cat((rgb, alpha), dim=-1)
 
+
+
+class FaceNerfPaperNeRFModelDualCond(torch.nn.Module):
+    r"""Implements the NeRF model as described in Fig. 7 (appendix) of the
+    arXiv submission (v0). """
+
+    def __init__(
+        self,
+        num_layers=8,
+        hidden_size=256,
+        skip_connect_every=4,
+        num_encoding_fn_xyz=6,
+        num_encoding_fn_dir=4,
+        num_encoding_fn_ldmks=4,
+        include_input_xyz=True,
+        include_input_dir=True,
+        include_input_ldmks=True,
+        use_viewdirs=True,
+        use_expression=True,
+        use_landmarks3d: bool = True,
+        use_appearance_code: bool =True,
+        use_deformation_code: bool = True,
+        num_train_images: int = 0,
+        embedding_vector_dim=32,
+        landmarks3d_last=False,
+        encode_ldmks3d=False,
+
+    ):
+        super(FaceNerfPaperNeRFModelDualCond, self).__init__()
+        include_input_xyz = 3 if include_input_xyz else 0
+        include_input_dir = 3 if include_input_dir else 0
+        include_input_ldmks = 1 if include_input_ldmks else 0
+
+        include_expression = 50 if use_expression else 0
+        include_landmarks3d = 68 if use_landmarks3d else 0
+
+        self.landmarks3d_last = landmarks3d_last
+
+        self.dim_xyz = include_input_xyz + 2 * 3 * num_encoding_fn_xyz
+        self.dim_dir = include_input_dir + 2 * 3 * num_encoding_fn_dir
+        self.dim_expression = include_expression # + 2 * 3 * num_encoding_fn_expr
+        self.dim_landmarks3d = include_input_ldmks*include_landmarks3d + 2 * include_landmarks3d * num_encoding_fn_ldmks + include_landmarks3d*3
+        self.dim_full_landmarks3d = self.dim_landmarks3d
+
+        self.encode_ldmks3d = encode_ldmks3d
+        if self.encode_ldmks3d:
+            self.layers_ldmks3d_enc = torch.nn.ModuleList()
+            self.layers_ldmks3d_enc.append(torch.nn.Linear(self.dim_landmarks3d+self.dim_xyz, 128))
+            self.layers_ldmks3d_enc.append(torch.nn.Linear(128, 128))
+            self.layers_ldmks3d_enc.append(torch.nn.Linear(128+self.dim_expression+embedding_vector_dim//2, 128))
+            self.layers_ldmks3d_enc.append(torch.nn.Linear(128, 3))
+            torch.nn.init.uniform_(self.layers_ldmks3d_enc[-1].weight, a=-1e-4, b=1e-4)
+            self.dim_landmarks3d = 0
+
+            self.layers_background_enc = torch.nn.ModuleList()
+            self.layers_background_enc.append(torch.nn.Linear(self.dim_xyz+embedding_vector_dim//2, 128))
+            self.layers_background_enc.append(torch.nn.Linear(128, 128))
+            self.layers_background_enc.append(torch.nn.Linear(128+self.dim_xyz+embedding_vector_dim//2, 128))
+            self.layers_background_enc.append(torch.nn.Linear(128, 3))
+            torch.nn.init.uniform_(self.layers_background_enc[-1].weight, a=-1e-4, b=1e-4)
+
+        # add appearance code
+        self.use_appearance_code = use_appearance_code
+        self.use_deformation_code = use_deformation_code
+        self.dim_appearance_codes = embedding_vector_dim if use_appearance_code else 0
+        self.dim_deformation_codes = embedding_vector_dim if use_deformation_code else 0
+        # self.dim_latent_code = embedding_vector_dim
+
+        self.layers_xyz = torch.nn.ModuleList()
+        self.use_viewdirs = use_viewdirs
+        self.use_landmarks3d = use_landmarks3d
+
+        # dim of the first input group to predict the density
+        input_density_dim = self.dim_xyz
+
+        self.layers_xyz.append(torch.nn.Linear(input_density_dim, 256))
+        for i in range(1, 6):
+            if i == 3:
+                self.layers_xyz.append(torch.nn.Linear(input_density_dim + 256, 256))
+            else:
+                self.layers_xyz.append(torch.nn.Linear(256, 256))
+        self.fc_feat = torch.nn.Linear(256, 256)
+        self.fc_alpha = torch.nn.Linear(256, 1)
+
+        self.layers_dir = torch.nn.ModuleList()
+
+        # dim of the second input group to predict the color
+        input_color_dim = self.dim_dir + self.dim_appearance_codes
+        if landmarks3d_last:
+            input_color_dim += self.dim_landmarks3d
+
+        self.layers_dir.append(torch.nn.Linear(256 + input_color_dim, 128))
+        for i in range(3):
+            self.layers_dir.append(torch.nn.Linear(128, 128))
+        self.fc_rgb = torch.nn.Linear(128, 3)
+        self.relu = torch.nn.functional.relu
+
+    def forward(self, x,  expression=None, appearance_codes=None, deformation_codes=None, cutoff_ws=None, pos_enc_func=None, **kwargs):
+        if self.use_landmarks3d:
+            # xyz: [pos_enc(ldmks); pos_enc(XYZ)]
+            xyz, dirs = x[..., : self.dim_full_landmarks3d+self.dim_xyz], x[..., self.dim_full_landmarks3d+self.dim_xyz :]
+            # xyz_pts: [XYZ] = 3
+            xyz_pts = xyz[..., self.dim_full_landmarks3d : self.dim_full_landmarks3d+3]
+            # xyz_enc = [pos_enc(ldmks)]
+            xyz_enc = xyz[..., self.dim_full_landmarks3d :]
+
+            if not self.landmarks3d_last:
+                expr_encoding = (expression * 1 / 3).repeat(x.shape[0], 1)
+                deformation_codes_face = deformation_codes[:deformation_codes.shape[-1]//2].repeat(xyz.shape[0], 1)
+                expr_encoding_deformation_face = torch.cat((expr_encoding, deformation_codes_face), axis=1)
+                delta_ldmks = self.delta_ldmks(xyz, expr_encoding_deformation_face)
+
+                deformation_codes_background = deformation_codes[deformation_codes.shape[-1]//2:].repeat(xyz.shape[0], 1)
+                xyz_deform_background = torch.cat((xyz_enc, deformation_codes_background), axis=1)
+                delta_background = self.delta_background(xyz_deform_background)
+
+                xyz = cutoff_ws[:, None]*(xyz_pts+delta_ldmks) + (1-cutoff_ws[:, None])*(xyz_pts+delta_background)
+                
+                xyz = pos_enc_func(xyz, None, None)
+        else:
+            raise NotImplementedError
+    
+
+        initial = xyz
+
+        # appearance_codes = appearance_codes.repeat(xyz.shape[0], 1)
+        x = initial
+                   
+        for i in range(6):
+            if i == 3:
+                x = self.layers_xyz[i](torch.cat((initial, x), -1))
+            else:
+                x = self.layers_xyz[i](x)
+            x = self.relu(x)
+        feat = self.fc_feat(x)
+        alpha = self.fc_alpha(feat)
+
+        if self.use_viewdirs:
+            if self.use_appearance_code:
+                appearance_codes = appearance_codes.repeat(xyz.shape[0], 1)
+                x = self.layers_dir[0](torch.cat((feat, dirs, appearance_codes), -1))
+            else:
+                x = self.layers_dir[0](torch.cat((feat, dirs), -1))
+        else:
+            x = self.layers_dir[0](feat)
+        x = self.relu(x)
+        for i in range(1, 3):
+            x = self.layers_dir[i](x)
+            x = self.relu(x)
+        rgb = self.fc_rgb(x)
+        return torch.cat((rgb, alpha), dim=-1)
+
+    def delta_ldmks(self, xyz_ldmks, expression):
+        if self.encode_ldmks3d:
+            for i in range(len(self.layers_ldmks3d_enc)):
+                if i == 2:
+                    xyz_ldmks = torch.cat((xyz_ldmks, expression), dim=1)
+                xyz_ldmks = self.layers_ldmks3d_enc[i](xyz_ldmks)
+                if i < len(self.layers_ldmks3d_enc)-1:
+                    xyz_ldmks = self.relu(xyz_ldmks)
+        return xyz_ldmks
+    
+    def delta_background(self, xyz_deform_background):
+        start_xyz = xyz_deform_background
+        for i in range(len(self.layers_background_enc)):
+            if i == 2:
+                xyz_deform_background = torch.cat((xyz_deform_background, start_xyz), dim=1)
+            xyz_deform_background = self.layers_background_enc[i](xyz_deform_background)
+            if i < len(self.layers_background_enc)-1:
+                xyz_deform_background = self.relu(xyz_deform_background)
+        return xyz_deform_background
+
