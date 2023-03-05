01cf79d8858f749490bf1f40ec38b8421a9b629e
cuevhv <cuevhv@yahoo.com>
2023-03-05 19:08:20 +0000

    comment out train_utils and raplace it for train_utils_simplified

--- nerf/__init__.py
+++ nerf/__init__.py
@@ -5,6 +5,9 @@ from .load_llff import load_llff_data
 from .models import *
 from .nerf_helpers import *
 from .nerface_helpers import get_ray_bundle as get_ray_bundle_nerface
-from .train_utils import *
+# from .train_utils import *
+from .train_utils_simplified import *
 from .volume_rendering_utils import *
 from .barf_utils import RefinePose
+from .nerf_base import NerfBase
+from .load_nerface_batch import NerfFaceDataset
